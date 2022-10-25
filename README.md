7c9bd491249f523db7c4485d85da49cc3cf6e1c3
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-24 20:00:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Console Log
+
+***September 1, 2021***
+
+Practice console logging in JS.
+
+## Used In This Repository
+
+- JavaScript 1.5
+- [Node.JS v19.0.0](https://nodejs.org/en/download/)
+
+## Getting Started
+
+To Run: 
+`node console.js`
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## Acknowledgments
+
+* [awesome-readme](https://github.com/matiassingers/awesome-readme)
---
+++ console.js
@@ -0,0 +1,36 @@
+// number
+console.log(12);
+
+// arithmetic
+console.log(5 + 5);
+
+// string
+console.log("This is a string.");
+
+// unary operation
+let unaryOperation = 10;
+++unaryOperation;
+console.log(unaryOperation);
+
+// boolean
+console.log(12 == 23);
+
+// logical operation
+console.log(3 != 5);
+
+// undefined
+let x;
+console.log(x);
+
+// Add a number and a string
+let str1 = "Hello";
+console.log(1 + str1);
+
+// Add a number and a string of a number
+let number = "2";
+let str2 = "Goodbye";
+console.log(number + str2);
+
+// Subtract a number from a string
+let str3 = "Hello Again";
+console.log(str3 - 2);
